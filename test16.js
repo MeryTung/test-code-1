f67c9fdd8ab3a585627f6971481a520cdf78145e
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-08-09 18:18:18 +0800

    test formatArrayChildren and normalizeArrayChildren

--- test16.js
+++ test16.js
@@ -5,48 +5,36 @@
       {id: 2, pid: 1, name: '朝阳区'},
       {id: 3, pid: 1, name: '东城区'},
       {id: 4, pid: 0, name: '山西省'},
+      {id: 7, pid: 2, name: '望京'},
       {id: 5, pid: 4, name: '太原市'},
       {id: 6, pid: 5, name: '杏花岭区'}
     ];
   
     function formatArrayChildren (arr, pid = 0) {
-      var res = [];
-      
-      for (var i = 0; i < arr.length; i += 1) {
-        var cur = arr[i];
-        
-        if (cur.pid === pid) {
-          cur.children = formatArrayChildren(arr, cur.id);
-          res = res.concat(cur);
+      let res = []
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i].pid === pid) {
+          arr[i].children = formatArrayChildren(arr, arr[i].id)
+          res = res.concat(arr[i])
         }
       }
-  
-      return res;
+      return res
     }
+    
+    const formatedArrayChildren = formatArrayChildren(cityList)
+    console.log(formatedArrayChildren)
+    console.log('-----------------')
   
-    var formatedCityList = formatArrayChildren(cityList);
-    console.log(formatedCityList);
-    console.log('-----------------');
-  
-    function normalizeArrayChildren (arr, pid = 0) {
-      var res = [];
-  
-      for (var i = 0; i < arr.length; i += 1) {
-        var cur = arr[i];
-  
-        res.push(cur);
-  
-        if (cur.pid === pid) {
-          res = res.concat(normalizeArrayChildren(cur.children, cur.id));
-        }
-  
-        delete cur.children;
+    function normalizeArrayChildren (arr) {
+      let res = []
+      for (let i = 0; i < arr.length; i++) {
+        res.push(arr[i])
+        res = res.concat(normalizeArrayChildren(arr[i].children))
+        delete arr[i].children
       }
-  
-      return res;
+      return res
     }
   
-    var $cityList = normalizeArrayChildren(formatedCityList);
-    console.log($cityList);
+    console.log(normalizeArrayChildren(formatedArrayChildren))
   
   })();
