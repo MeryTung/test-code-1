568e7ea354e9b4c9fdd76435d239ed1bf3f5ca92
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-08-12 11:53:25 +0800

    test compareVersion

--- test263.js
+++ test263.js
@@ -8,7 +8,6 @@
     s1 = s1.concat(new Array(maxLen - s1.length).fill(0))
     s2 = s2.concat(new Array(maxLen - s2.length).fill(0))
     
-
     for (let i = 0; i < maxLen; i++) {
       if (s1[i] - 0 < s2[i] - 0) {
         return -1
@@ -24,5 +23,6 @@
   console.log(compareVersion('1.3.0', '1.2.0'))
   console.log(compareVersion('1.2.1', '1.2.2'))
   console.log(compareVersion('1.2', '1.2.1'))
+  console.log(compareVersion('1.2', '1.2.0.1'))
 
 })();
