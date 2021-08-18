dc2bd4462e2e661fe1c7d43dd6442af145c0c573
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-08-18 18:54:18 +0800

    test data path

---
+++ test264.js
@@ -0,0 +1,85 @@
+
+;(function () {
+
+  const obj = {
+    a: {
+      b: {
+        c: {
+          d: {
+            list: [1, [{a: [-1, -2]}, [100, 200, 300]], 4]
+          }
+        }
+      }
+    },
+    e: [[[[100, 200]]]]
+  }
+
+  // { 'a.b': [1, 0, 5, 6] }
+  function $spliceData (data) {
+    const keys = Object.keys(data)
+    keys.forEach(key => {
+      let _obj
+      const _keys = key.split('.')
+      const arr = _keys.reduce((cur, pre) => {
+        _obj = cur
+        const reg = /([a-z]+)?(\[(\d+)\])/g
+        let matched
+        let mark = false
+        while (matched = reg.exec(pre)) {
+          mark = true
+          if (matched[1]) {
+            _obj = _obj[matched[1]]
+          }
+          if (matched[3]) {
+            _obj = _obj[matched[3]]
+          }
+        }
+        if (mark) return _obj
+        return _obj[pre]
+      }, obj)
+      arr.splice(data[key][0], data[key][1], ...data[key].slice(2))
+    })
+  }
+
+  $spliceData({
+    'a.b.c.d.list[1][0].a': [1, 0, ...[99, 98, 97, 96]],
+    'e[0][0][0]': [1, 0, ...[-1, -2, -3]]
+  })
+
+  console.log(obj.a.b.c.d.list[1][0].a)
+  console.log(obj.e[0][0][0])
+
+
+})();
+
+;(function () {
+  return
+
+   const str = 'a.b.c.d.list'
+ 
+  const arr = str.split('.')
+
+  const res = arr.reduce((cur, pre) => {
+    return cur[pre]
+  }, obj)
+
+  console.log(res)
+
+  res.splice(1, 0, ...[0, 1, 2, 3, 4])
+  
+  console.log(res)
+})();
+
+;(function () {
+  return
+
+  const str = 'list[0][1][2]'
+
+  const reg = /([a-z]+)?(\[(\d+)\])/g
+
+  let matched
+  while (matched = reg.exec(str)) {
+    console.log(matched)
+  }
+
+})();
