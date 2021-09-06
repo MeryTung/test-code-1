e0172ea6846bb5ff3a216d87a36130c94dfa7d81
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-09-06 12:24:06 +0800

    update _reduce

--- test35.js
+++ test35.js
@@ -1,5 +1,5 @@
 (function () {
-  // https://github.com/zymfe/test-code/blob/master/tes125.js
+  // https://github.com/zhaoyiming0803/test-code/blob/master/tes125.js
   var arr = [1, 2, 3, 4];
 
   // Array.prototype._reduce = function (cb, base) {
@@ -14,14 +14,28 @@
   // 	return res;
   // }
 
+  // Array.prototype._reduce = function (cb, base) {
+  //   var index = 0;
+  //   var res = base !== undefined ? base : this[index++];
+  //   for (; index < this.length; index++) {
+  //     res = cb(res, this[index]);
+  //   }
+  //   return res;
+  // };
+
+  // [1, 2, 3].reduce(() => {})
   Array.prototype._reduce = function (cb, base) {
-    var index = 0;
-    var res = base !== undefined ? base : this[index++];
-    for (; index < this.length; index++) {
-      res = cb(res, this[index]);
+    var len = this.length
+    if (!len) {
+      return this
     }
-    return res;
-  };
+    var index = 0
+    var res = base !== undefined ? base : this[index++]
+    while (index < len) {
+      res = cb(res, this[index++])
+    }
+    return res
+  }
 
   var sum = arr._reduce(function (a, b) {
     return a + b;
