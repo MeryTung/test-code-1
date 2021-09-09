a5a446be3dc829391bb0192eff9a8b3989d92d86
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-09-09 23:07:05 +0800

    chore: add comments of 'get most str'

--- test284.js
+++ test284.js
@@ -44,6 +44,8 @@ const str = '0sf0we0weeewe0w000gvfff'
 
   // 正则表达式中的小括号"()"。是代表分组的意思。 如果再其后面出现\1则是代表与第一个小括号中要匹配的内容相同
 
+  // ***** 连续出现了 5 次，说明总共有 6 个元素
+
   function getMostStr(str) {
     const arr = str.match(/(\w)\1+/g)
     const maxLen = Math.max(...arr.map(s => s.length))
