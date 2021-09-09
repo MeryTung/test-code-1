f02eb54b7ee9484e52a522f48be1fd95814ccea2
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-09-09 22:52:49 +0800

    test: get most str

---
+++ test284.js
@@ -0,0 +1,58 @@
+
+const str = '0sf0we0weeewe0w000gvfff'
+
+; (function () {
+
+  function getMostStr(str) {
+    let max = 1
+    let i = 0
+    const arr = []
+    for (; i < str.length;) {
+      let num = 1
+      let j = i + 1
+      let _str = str[i]
+      while (str[i] === str[j]) {
+        j++
+        num++
+        _str += str[i]
+      }
+      if (num >= max) {
+        arr.push(_str)
+        max = num
+        _str = ''
+      }
+      i += num
+    }
+    const maxLen = Math.max.apply(null, arr.map(item => item.length))
+    return arr.reduce((map, item) => {
+      if (item.length === maxLen) {
+        map[item[0]] = maxLen
+      }
+      return map
+    }, {})
+  }
+
+  console.log(getMostStr(str))
+
+})();
+
+; (function () {
+
+  // \1必须与小括号配合使用。
+
+  // 正则表达式中的小括号"()"。是代表分组的意思。 如果再其后面出现\1则是代表与第一个小括号中要匹配的内容相同
+
+  function getMostStr(str) {
+    const arr = str.match(/(\w)\1+/g)
+    const maxLen = Math.max(...arr.map(s => s.length))
+    return arr.reduce((map, item) => {
+      if (item.length === maxLen) {
+        map[item[0]] = maxLen
+      }
+      return map
+    }, {})
+  }
+
+  console.log(getMostStr(str))
+
+})();
