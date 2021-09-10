f561dabc7e651539d45415468d6259e83158a26d
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-09-10 21:34:16 +0800

    test binary search

--- test148.js
+++ test148.js
@@ -2,6 +2,7 @@
   // binary search
   // 二分查找的数组必须是有序列表
   // 对于连续的数字，可以直接用中间值作为二分，否则，需要用中间索引作为二分
+  // 将索引作为二分是比较靠谱的
   var arr = [1, 2, 3, 100, 4, 8, 101, 54, 93, 27, 65];
 
   // function search (arr, target) {
@@ -45,22 +46,38 @@
 
 ;(function () {
 
-  const arr = new Array(11).fill(0).map((item, index) => index)
+  const arr = [1, 4, 8, 10, 20, 77, 88, 91, 93, 100]
   
   function search (arr, target) {
-    let min = arr[0]
-    let max = arr[arr.length - 1]
+    // let min = arr[0]
+    // let max = arr[arr.length - 1]
     
-    while (min < max) {
-      let middle = Math.floor((min + max) / 2)
-      if (middle > target) {
-        max = middle
-      } else if (middle < target) {
-        min = middle
+    // while (min < max) {
+    //   let middle = Math.floor((min + max) / 2)
+    //   if (middle > target) {
+    //     max = middle
+    //   } else if (middle < target) {
+    //     min = middle
+    //   } else {
+    //     return middle
+    //   }
+    // }
+
+    while (arr.length > 1) {
+      const minIndex = 0
+      const maxIndex = arr.length - 1
+      const middleIndex = Math.floor((minIndex + maxIndex) / 2)
+      if (arr[middleIndex] > target) {
+        arr = arr.slice(minIndex, middleIndex)
+      } else if (arr[middleIndex] < target) {
+        arr = arr.slice(middleIndex)
       } else {
-        return middle
+        return arr[middleIndex]
       }
     }
+
+    if (target === arr[0]) return target
+    
 
     // if (arr.length <= 1) {
     //   if (target === arr[0]) return target
