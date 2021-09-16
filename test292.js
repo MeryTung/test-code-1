a800ca325e64e6a9dd69ceae8dace0d1654ae8fb
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-09-16 15:12:16 +0800

    test: search max child arr length from array

---
+++ test292.js
@@ -0,0 +1,35 @@
+;(function () {
+
+  // 最长上升子序列：给定一个无序的整数数组，找到其中最长上升子序列的长度
+
+  const arr = [2, 3, 5, 1, 8, 9, 4, 6, 7, 12, 20, 0, 1, 2, 3]
+
+  function searchMaxLength (arr) {
+    let i =  0
+    const childList = []
+    for (; i < arr.length;) {
+      let j = i + 1
+      const tempList = [arr[i]]
+      while (arr[j] > arr[i]) {
+        tempList.push(arr[j])
+        i += 1
+        j += 1
+      }
+      if (childList.length === 0 || tempList.length >= childList[childList.length - 1].length) {
+        childList.push(tempList)
+      }
+      i = j
+    }
+
+    const maxLength = Math.max(...childList.map(item => item.length))
+    return childList.reduce((list, childItem) => {
+      if (childItem.length === maxLength) {
+        return list.concat([childItem])
+      }
+      return list
+    }, [])
+  }
+
+  console.log(searchMaxLength(arr))
+
+})();
