13b0d956f86eb82cc30f9aafff726b6f57d63bc4
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-09-18 15:29:41 +0800

    test: get value by data path

--- test295.js
+++ test295.js
@@ -3,7 +3,9 @@
   const obj = {
     a: 1,
     b: {
-      c: 1
+      c: {
+        d: 123
+      }
     }
   }
 
@@ -14,6 +16,9 @@
     let res
 
     function _get(value) {
+      if (i === arr.length) {
+        return (res = value)
+      }
       if (typeof value === 'object') {
         const keys = Object.keys(value)
         for (let j = 0; j < keys.length; j++) {
@@ -40,7 +45,57 @@
   }
 
   console.log(get(obj, 'b.c'))
+  console.log(get(obj, 'b.c.d'))
+  console.log(get(obj, 'b.c.d.e'))
   console.log(get(obj, 'a.b2.c'))
 
+  console.log('------------------------')
+})();
+
+; (function () {
+
+  const obj = {
+    a: 1,
+    b: {
+      c: {
+        d: 123
+      }
+    }
+  }
+
+
+  function get(path) {
+    const arr = path.split('.')
+    let i = 0
+    let res
+
+    return function _get(value) {
+      if (i === arr.length) {
+        res = value
+      } else if (typeof value === 'object') {
+        const keys = Object.keys(value)
+        for (let j = 0; j < keys.length; j++) {
+          const _key = keys[j]
+          if (_key === arr[i]) {
+            i++
+            return arguments.callee(value[_key])
+          }
+        }
+        res = 'error'
+      } else {
+        if (i !== arr.length) {
+          res = 'error'
+        } else {
+          res = value
+        }
+      }
+      return res
+    }
+  }
+
+  console.log(get('b.c')(obj))
+  console.log(get('b.c.d')(obj))
+  console.log(get('acb.c')(obj))
+  console.log(get('b.c.d.e')(obj))
 
 })();
