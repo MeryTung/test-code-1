c5e6c22443d726136f51f0dc76d3fb97fb695017
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-10 23:07:08 +0800

    chore: update searchMaxLength

--- test292.js
+++ test292.js
@@ -2,7 +2,7 @@
 
   // 最长上升子序列：给定一个无序的整数数组，找到其中最长上升子序列的长度
 
-  const arr = [2, 3, 5, 1, 8, 9, 4, 6, 7, 12, 20, 0, 1, 2, 3]
+  const arr = [2, 3, 5, 1, 8, 9, 4, 6, 7, 12, 0, 1, 2, 3]
 
   function searchMaxLength (arr) {
     let i =  0
@@ -10,9 +10,8 @@
     for (; i < arr.length;) {
       let j = i + 1
       const tempList = [arr[i]]
-      while (arr[j] > arr[i]) {
+      while (arr[j] > tempList[tempList.length - 1]) {
         tempList.push(arr[j])
-        i += 1
         j += 1
       }
       if (childList.length === 0 || tempList.length >= childList[childList.length - 1].length) {
