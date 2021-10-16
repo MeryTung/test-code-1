984157354dfea60baabb40c6e597d8ac8e2b952b
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-16 17:04:25 +0800

    test event loop different between node and browser

--- test207-event-loop.js
+++ test207-event-loop.js
@@ -1,16 +1,19 @@
-// https://github.com/zymfe/test-code/blob/master/test29.js
-// https://github.com/zymfe/test-code/blob/master/test72.js
+// https://github.com/zhaoyiming0803/test-code/blob/master/test29.js
+// https://github.com/zhaoyiming0803/test-code/blob/master/test72.js
+// https://zhuanlan.zhihu.com/p/371027812
 
 ~(function () {
-  setTimeout(() => {
-    console.log("setTimeout1");
-    Promise.resolve().then(() => console.log("promise1"));
-  });
+  return
 
   setTimeout(() => {
-    console.log("setTimeout2");
-    Promise.resolve().then(() => console.log("promise2"));
-  });
+    console.log("setTimeout1")
+    Promise.resolve().then(() => console.log("promise1"))
+  })
+
+  setTimeout(() => {
+    console.log("setTimeout2")
+    Promise.resolve().then(() => console.log("promise2"))
+  })
 
   // node version v10.20.1
   // setTimeout1 setTimeout2 promise1 promise2
@@ -21,16 +24,46 @@
 })();
 
 ~(function () {
-  process.nextTick(() => {
-    console.log("nextTick11");
-    Promise.resolve().then(() => console.log("promise11"));
-  });
+
+  // 这个函数其实是独立于 Event Loop 之外的，它有一个自己的队列，当 node event loop 每个阶段完成后，
+  // 如果存在 nextTick 队列，就会清空队列中的所有回调函数，并且优先于其他 microtask 执行
+  
+  return
 
   process.nextTick(() => {
-    console.log("nextTick22");
-    Promise.resolve().then(() => console.log("promise22"));
-  });
+    console.log("nextTick11")
+    Promise.resolve().then(() => console.log("promise11"))
+  })
+
+  process.nextTick(() => {
+    console.log("nextTick22")
+    Promise.resolve().then(() => console.log("promise22"))
+  })
 
   // nextTick 的优先级比 promise 高
   // nextTick11 nextTick22 promise11 promise22
+})();
+
+;(function () {
+
+  setTimeout(() => console.log('setTimeout'))
+
+  Promise.resolve().then(() => {
+    console.log('Promise')
+  })
+
+  process.nextTick(() => {
+    console.log('nextTick1')
+
+    process.nextTick(() => {
+      console.log('nextTick2')
+
+      process.nextTick(() => {
+        console.log('nextTick3')
+      })
+    })
+  })
+
+  // nextTick1 nextTick2 nextTick3 Promise setTimeout
+
 })();
--- test72.js
+++ test72.js
@@ -2,6 +2,8 @@
 // https://github.com/zhaoyiming0803/test-code/blob/master/test207-event-loop.js
 
 (function () {
+  return
+
   setTimeout(function () {
     console.log(0);
   });
@@ -22,11 +24,46 @@
 
   // 2 4 3 0 1
 
-  // 优先级顺序：process.nextTick > Promise > setTimeout/setInterval > setImmediate
+  // 优先级顺序（event loop in node）：process.nextTick > Promise > setTimeout/setInterval > setImmediate
 
   // 参考文档：
+  // ***** https://zhuanlan.zhihu.com/p/371027812
   // https://www.jianshu.com/p/837b584e1bdd
   // https://www.zhihu.com/question/23028843/answer/34585034
   // https://nodejs.org/en/blog/release/v0.10.0/
   // https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/
+})();
+
+; (function () {
+
+  return
+
+  const fs = require('fs')
+
+  fs.readFile(__filename, () => {
+    setTimeout(() => {
+      console.log('timeout')
+    }, 0)
+    setImmediate(() => {
+      console.log('immediate')
+    })
+  })
+
+  // 执行顺序：immediate timeout
+
+})();
+
+; (function () {
+
+  setTimeout(() => {
+    console.log('timeout')
+  }, 1)
+  setImmediate(() => {
+    console.log('immediate')
+  })
+
+  // 对于以上代码来说，setTimeout 可能执行在前，也可能执行在后。
+  // 首先 setTimeout(fn, 0) === setTimeout(fn, 1)，这是由源码决定的，进入事件循环也是需要成本的，如果在准备时候花费了大于 1ms 的时间，那么在 timer 阶段就会直接执行 setTimeout 回调
+  // 如果准备时间花费小于 1ms，那么就是 setImmediate 回调先执行了
+
 })();
