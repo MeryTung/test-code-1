ca6511c3d92e21d043cc9b41ce02ad1ee47ca088
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-16 17:41:19 +0800

    test: getMostStr

--- test284.js
+++ test284.js
@@ -6,25 +6,24 @@ const str = '0sf0we0weeewe0w000gvfff'
 ; (function () {
 
   function getMostStr(str) {
-    let max = 1
-    let i = 0
     const arr = []
-    for (; i < str.length;) {
-      let num = 1
+    let max = 1
+    for (let i = 0; i < str.length;) {
       let j = i + 1
       let _str = str[i]
       while (str[i] === str[j]) {
+        _str += str[j]
         j++
-        num++
-        _str += str[i]
       }
-      if (num >= max) {
+      const len = _str.length
+      if (len >= max) {
+        max = len
         arr.push(_str)
-        max = num
         _str = ''
       }
-      i += num
+      i = j
     }
+    
     const maxLen = Math.max.apply(null, arr.map(item => item.length))
     return arr.reduce((map, item) => {
       if (item.length === maxLen) {
@@ -42,9 +41,9 @@ const str = '0sf0we0weeewe0w000gvfff'
 
   // \1必须与小括号配合使用。
 
-  // 正则表达式中的小括号"()"。是代表分组的意思。 如果再其后面出现\1则是代表与第一个小括号中要匹配的内容相同
+  // 正则表达式中的小括号"()"。是代表分组的意思。 如果在其后面出现 \1 则是代表与第一个小括号中要匹配的内容相同
 
-  // ***** 连续出现了 5 次，说明总共有 6 个元素
+  // ***** 连续出现了 5 次，说明总共出现 6 次
 
   function getMostStr(str) {
     const arr = str.match(/(\w)\1+/g)
