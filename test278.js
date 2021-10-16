637a9351a8406d1e16c6a2f2287f1f753fe250e2
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-16 23:55:05 +0800

    chore: update mockInstanceof

--- test278.js
+++ test278.js
@@ -1,19 +1,19 @@
 ;(function () {
 
-  function mockInstanceof (obj, Fn) {
-    if (Fn === null || Fn === undefined || typeof Fn !== 'function') {
+  function mockInstanceof (a, b) {
+    if (b === null || b === undefined || typeof b !== 'function') {
       return false
     }
 
-    let prototype = Fn.prototype
+    const prototype = b.prototype
     // instanceof 运算符用来测试一个对象在其原型链中是否存在一个构造函数的 prototype 属性
     // 参数：object（要检测的对象）instanceof constructor（某个构造函数）
     // 描述：instanceof 运算符用来检测 constructor.prototype 是否存在于参数 object 的原型链上。
-    while (obj.__proto__) {
-      if (obj.__proto__ === prototype) {
+    while (a.__proto__) {
+      if (a.__proto__ === prototype) {
         return true
       }
-      obj = obj.__proto__
+      a = a.__proto__
     }
     
     return false
