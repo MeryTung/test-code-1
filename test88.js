bb9677f63f06cfb4b1b3b7b915c8a5292a69614b
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-16 23:20:06 +0800

    chore: update mergeSort

--- test88.js
+++ test88.js
@@ -2,49 +2,47 @@
 
   function mergeSort (arr) {
     if (arr.length === 1) {
-      return arr;
+      return arr
     }
 
-    var middle = Math.floor(arr.length / 2);
-    var left = arr.slice(0, middle);
-    var right = arr.slice(middle);
-
-    console.log('left', left);
-    console.log('right', right);
+    var middle = Math.floor(arr.length / 2)
+    var left = arr.slice(0, middle)
+    var right = arr.slice(middle)
 
     var merged = merge(mergeSort(left), mergeSort(right))
 
-    console.log('merged', merged);
+    console.log('merged', merged)
 
-    return merged;
+    return merged
   }
 
   function merge (left /* [2, 5] */, right /* [3, 4] */) {
-    var res = [];
-    var i = 0; 
-    var j = 0;
-    var leftLen = left.length;
-    var rightLen = right.length;
+    var res = []
+    var i = 0 
+    var j = 0
+    var leftLen = left.length
+    var rightLen = right.length
 
     while (i < leftLen && j < rightLen) {
       if (left[i] < right[j]) {
-        res.push(left[i++]);
+        res.push(left[i++])
       } else {
-        res.push(right[j++]);
+        res.push(right[j++])
       }
     }
     
     while (i < leftLen) {
-      res.push(left[i++]);
+      res.push(left[i++])
     }
 
     while (j < rightLen) {
-      res.push(right[j++]);
+      res.push(right[j++])
     }
 
-    return res;
+    return res
   }
 
-  console.log(mergeSort([100, 9, 10, 0, 3, 4, 2]));
+  console.log(mergeSort([100, 9, 10, 0, 3, 4, 2]))
+  console.log(merge([2, 5], [3, 4, 6]))
 
 })();
