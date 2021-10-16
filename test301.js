77a8de69a0880a8de5f3b077b085463ce18b7041
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-16 22:34:56 +0800

    test: delayering array

---
+++ test301.js
@@ -0,0 +1,32 @@
+;(function () {
+
+  /**
+   * 扁平化数组，适合对元素类型不敏感并且每个元素都是基本数据类型的场景
+   * @param {*} arr 
+   * @returns 
+   */
+  function formatArr1 (arr) {
+    return arr.toString().split(',')
+  }
+
+  console.log(formatArr1([1, 2, [3, 4, 5, [6, 7, 8, [9, [10]]]]]))
+
+  /**
+   * 通用
+   * @param {*} arr 
+   */
+  function formatArr2 (arr) {
+    let res = []
+    arr.forEach(item => {
+      if (Array.isArray(item)) {
+        res = res.concat(formatArr2(item))
+      } else {
+        res.push(item)
+      }
+    })
+    return res
+  }
+
+  console.log(formatArr2([1, 2, [3, 4, 5, [6, 7, 8, [9, [10]], {a: 1}, new Date()]]]))
+
+})();
