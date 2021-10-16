436f889e26068413645f231648c6ff3106461eec
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-16 17:59:52 +0800

    test: mapDataPathAndValue

--- test283.js
+++ test283.js
@@ -30,17 +30,20 @@
     const type = value => Object.prototype.toString.call(value).replace(/\[\w+\s(\w+)\]/, ($0, $1) => $1)
 
     function buildMap (key, value) {
-      key = Array.isArray(key) ? key : [key]
+      // key = Array.isArray(key) ? key : [key]
       if (type(value) === 'Object') {
         Object.keys(value).forEach(_key => {
-          buildMap(key.concat(_key), value[_key])
+          // buildMap(key.concat(_key), value[_key])
+          buildMap(key + _key, value[_key])
         })
       } else {
-        map[key.join('.')] = value
+        // map[key.join('.')] = value
+        map[key] = value
       }
     }
 
-    buildMap([], data)
+    // buildMap([], data)
+    buildMap('', data)
 
     return map
   }
