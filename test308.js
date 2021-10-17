7edfde1c71bd78e380eb3b37e85ff2a59b156021
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-18 00:27:50 +0800

    test: mapDataPathAndValue and   console.log(getValueByPath('a.d.e', data)) // { f: 200 }

---
+++ test308.js
@@ -0,0 +1,49 @@
+const data = {
+  a: { 
+    b: { 
+      c: 100 
+    },
+    d: {
+      e: {
+        f: 200
+      }
+    }
+  },
+  g: {
+    h: 300
+  }
+}
+
+; (function () {
+
+  function getValueByPath (path, data) {
+    let val = data
+    const arr = path.split('.')
+    while (arr.length && val[arr[0]]) {
+      val = val[arr.shift()]
+    }
+    return val
+  }
+
+  console.log(getValueByPath('a.b.c', data)) // 100
+  console.log(getValueByPath('a.d.e', data)) // { f: 200 }
+
+})();
+
+;(function () {
+
+  function mapDataPathAndValue (data, path = [], map = {}) {
+    Object.keys(data).forEach(key => {
+      const _path = path.concat(key)
+      if (typeof data[key] === 'object') {
+        mapDataPathAndValue(data[key], _path, map)
+      } else {
+        map[_path.join('.')] = data[key]
+      }
+    })
+    return map
+  }
+
+  console.log(mapDataPathAndValue(data)) // { 'a.b.c': 100, 'a.d.e.f': 200, 'g.h': 300 }
+
+})();
