3611bafded05f082f8cf0b86bd40bc4d6fac1767
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-17 15:31:22 +0800

    update: parasitic combination inheritance

--- test196.js
+++ test196.js
@@ -6,48 +6,47 @@
 
   Object._create = function (obj) {
     // function Obj() {}
-    // Obj.prototype = obj;
-    // Obj.prototype.constructor = Obj;
-    // return new Obj();
-    const _obj = {};
-    _obj.__proto__ = obj;
-    return _obj;
-  };
+    // Obj.prototype = obj
+    // Obj.prototype.constructor = Obj
+    // return new Obj()
+    const _obj = {}
+    _obj.__proto__ = obj
+    return _obj
+  }
 
-  function inhitance(Sub, Super) {
-    // const prototype = Object._create(Super.prototype);
-    // prototype.constructor = Sub;
-    // Sub.prototype = prototype;
+  function inherit(Sub, Super) {
+    Sub.prototype = Object._create(Super.prototype)
+    Sub.prototype.constructor = Sub
 
-    function Inner() {}
-    Inner.prototype = Super.prototype;
-    Inner.consructor = Inner;
-    Sub.prototype = new Inner();
+    // function Inner() {}
+    // Inner.prototype = Super.prototype
+    // Inner.consructor = Inner
+    // Sub.prototype = new Inner()
   }
 
   function SuperType(name) {
-    this.name = name;
+    this.name = name
   }
 
   SuperType.prototype.getName = function getName() {
-    return this.name;
-  };
-
-  function SubType(name, age) {
-    SuperType.call(this, name);
-    this.age = age;
+    return this.name
   }
 
-  inhitance(SubType, SuperType);
+  function SubType(name, age) {
+    SuperType.call(this, name)
+    this.age = age
+  }
 
-  const a = new SubType("zhangsan", 18);
-  console.log(a.getName()); // zhangsan
+  inherit(SubType, SuperType)
+
+  const a = new SubType("zhangsan", 18)
+  console.log(a.getName()) // zhangsan
 
   SubType.prototype.getAge = function getAge() {
-    return this.age;
-  };
+    return this.age
+  }
 
-  console.log(a.getAge()); // 18
-  // console.log(SuperType.prototype.getAge); // [Function: getAge] 不使用Inner
-  console.log(SuperType.prototype.getAge); // undefined 使用Inner
-})();
+  console.log(a.getAge()) // 18
+  console.log(SuperType.prototype.getAge) // [Function: getAge] 不使用Inner
+  console.log(SuperType.prototype.getAge) // undefined 使用Inner
+})()
