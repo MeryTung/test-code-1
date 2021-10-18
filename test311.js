e3c8803d4a47f820c6ace393ad44ebf244408a8b
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-18 22:38:18 +0800

    test: getShortestToChar

---
+++ test311.js
@@ -0,0 +1,31 @@
+;(function () {
+  // 求字符的最短距离
+  // 给定一个字符串 S 和一个字符 C。返回一个代表字符串 S 中每个字符到字符串 S 中的字符 C 的最短距离的数组。
+
+  const s = 'loveleetcode'
+  const c = 'e'
+
+  function getShortestToChar (s, c) {
+    const charArr = [] // [ 3, 5, 6, 11 ]
+    const reg = new RegExp(c, 'g')
+    let res = null
+    while (res = reg.exec(s)) {
+      charArr.push(res.index)
+    }
+    
+    const sArr = s.split('')
+    const arr = []
+    sArr.forEach((item, index) => {
+      let temp = s.length
+      charArr.forEach((item1, index1) => {
+        temp = Math.min(Math.abs(item1 - index), temp)
+      })
+      arr.push(temp)
+    })
+
+    return arr
+  }
+
+  console.log(getShortestToChar(s, c))
+
+})();
