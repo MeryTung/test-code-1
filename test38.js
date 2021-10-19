8ed8b95f2c137439a351577dc1314acc967c2077
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-19 14:42:55 +0800

    update: find substr nums and idxs

--- test38.js
+++ test38.js
@@ -1,26 +1,47 @@
-;(function () {
+var str = '每一段路都是一种领悟，每一段路都荆棘密布！'
 
-    var str = '每一段路都是一种领悟，每一段路都荆棘密布！';
+; (function () {
 
-    String.prototype._find = function (str) {
+    function findSubStr(str, subStr) {
         var res = {
             num: 0, // 当前字符在整个字符串中出现了几次
             idxs: [] // 分别出现在哪几个位置
-        };
-        
-        var i = 0;
-        var idx = this.indexOf(str, i);
-
-        while (idx > -1) {
-            res.num += 1;
-            res.idxs.push(idx);
-            i = idx + 1;
-            idx = this.indexOf(str, i);
         }
 
-        return res;
-    };
+        var idx = str.indexOf(subStr, 0)
 
-    console.log(str._find('一'));
-    
+        while (idx > -1) {
+            res.num += 1
+            res.idxs.push(idx)
+            idx = str.indexOf(subStr, idx + 1)
+        }
+
+        return res
+    }
+
+    console.time('findSubStr1')
+    console.log(findSubStr(str, '一'))
+    console.timeEnd('findSubStr1')
+
+})();
+
+;(function () {
+
+    function findSubStr (str, subStr) {
+        const res = {
+            num: 0,
+            idxs: []
+        }
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === subStr) {
+                res.num += 1
+                res.idxs.push(i)
+            }
+        }
+        return res
+    }
+
+    console.time('findSubStr2')
+    console.log(findSubStr(str, '一'))
+    console.timeEnd('findSubStr2')
 })();
