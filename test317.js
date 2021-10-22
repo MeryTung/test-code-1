010cb0d97761acbfd95f4a8ca9c0a4d39f4240f5
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-10-22 13:26:16 +0800

    test: The sum of three numbers is closest to a certain value

--- test317.js
+++ test317.js
@@ -2,8 +2,6 @@
 
   // 三数之和最接近某个值
 
-  const arr = [0, 4, 2, 9, 10, 7, 6, 5]
-
   function sum (arr, target) {
     const _arr = []
     for (let i = 0; i < arr.length; i++) {
@@ -21,7 +19,10 @@
       }
     }
 
-    const minStep = Math.min(..._arr.map(item => Math.abs(item.reduce((a, b) => a + b) - target, 0)))
+    const minStep = Math.min(..._arr.map(item => {
+      const sum = item.reduce((a, b) => a + b, 0)
+      return Math.abs(sum - target)
+    }))
     
     return _arr.reduce((res, item) => {
       const sum = item.reduce((a, b) => a + b, 0)
@@ -32,6 +33,39 @@
     }, [])
   }
 
-  console.log(sum(arr,30))
+  console.log(sum([-1, 2, 1, -4], 1))
+  console.log(sum([0, 4, 2, 9, -8, 7, -3, 6, 5], 1))
+  console.log(sum([0, 4, 2, 9, -8, 7, -3, 6, 5], -1))
 
+})();
+
+;(function () {
+
+  function sum (arr, target) {
+    arr.sort((a, b) => a - b)
+    let minSum = 0
+    for (let i = 0; i < arr.length; i++) {
+      let left = i + 1
+      let right = arr.length - 1
+      while (left < right) {
+        const _sum = arr[i] + arr[left] + arr[right]
+        if (_sum > target) {
+          right--
+        } else if (_sum < target) {
+          left++
+        } else {
+          return _sum
+        }
+        if (Math.abs(_sum - target) < Math.abs(minSum - target)) {
+          minSum = _sum
+        }
+      }
+    }
+
+    return minSum
+  }
+
+  console.log(sum([-1, 2, 1, -4], 1))
+  console.log(sum([0, 4, 2, 9, -8, 7, -3, 6, 5], 1))
+  console.log(sum([0, 4, 2, 9, -8, 7, -3, 6, 5], -1))
 })();
