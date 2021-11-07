55840ad4a169204ed5494d9f7e74257dede70dfa
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-11-08 02:58:34 +0800

    test: knapsack

---
+++ test328.js
@@ -0,0 +1,93 @@
+const list = [{
+  price: 400,
+  weight: 5
+}, {
+  price: 500,
+  weight: 5
+}, {
+  price: 200,
+  weight: 3
+}, {
+  price: 300,
+  weight: 4
+}, {
+  price: 350,
+  weight: 3
+}]
+
+;(function () {
+
+  return
+
+  // 使用贪心算法解决背包问题局部最优解
+
+  function getHighestValue (list, capacity) {
+    const _list = list.map(item => {
+      return {
+        ...item,
+        ratio: item.price / item.weight
+      }
+    })
+    
+    _list.sort((a, b) => b.ratio - a.ratio)
+
+    let currentCapacity = 0
+    let i = 0
+    while (currentCapacity < capacity && i < _list.length) {
+      const remainedCapacity = capacity - currentCapacity
+      const { weight, price } = _list[i]
+      if (remainedCapacity >= weight) {
+        currentCapacity += weight
+        i++
+      } else {
+        currentCapacity += weight * (remainedCapacity / weight)
+      }
+    }
+
+    return currentCapacity
+  }
+
+  console.log('背包的最大价值：', getHighestValue(list, 10))
+
+})();
+
+;(function () {
+
+  function getHighestValue (list, capacity) {
+    // 商品数量
+    const n = list.length
+    // 商品价格
+    const p = list.map(item => item.price)
+    // 商品重量
+    const w = list.map(item => item.weight)
+
+    // 只有一件商品时
+    let preResults = []
+    for (let i = 0; i < capacity; i++) {
+      if (i < w[0]) {
+        preResults[i] = 0
+      } else {
+        preResults[i] = p[0]
+      }
+    }
+
+    // 计算多件商品
+    let result = []
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < capacity; j++) {
+        if (j + 1 < w[i]) {
+          result[j] = preResults[j]
+        } else {
+          // .... 带优化
+          result[j] = Math.max(preResults[j], preResults[Math.abs(j - w[i])] + p[i])
+        }
+      }
+      preResults = result
+    }
+
+    return result
+  }
+
+  console.log(getHighestValue(list, 10))
+
+})();
