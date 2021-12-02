c8e3f3a6872a8eeaaf0142f701d57b8e9bdbb3e7
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-12-02 22:24:29 +0800

    test: getShortestToChar

--- test311.js
+++ test311.js
@@ -1,9 +1,10 @@
-;(function () {
-  // 求字符的最短距离
-  // 给定一个字符串 S 和一个字符 C。返回一个代表字符串 S 中每个字符到字符串 S 中的字符 C 的最短距离的数组。
+// 求字符的最短距离
+// 给定一个字符串 S 和一个字符 C。返回一个代表字符串 S 中每个字符到字符串 S 中的字符 C 的最短距离的数组。
 
-  const s = 'loveleetcode'
-  const c = 'e'
+const s = 'loveleetcode'
+const c = 'e'
+
+;(function () {
 
   function getShortestToChar (s, c) {
     const charArr = [] // [ 3, 5, 6, 11 ]
@@ -26,6 +27,38 @@
     return arr
   }
 
+  console.time('getShortestToChar1')
   console.log(getShortestToChar(s, c))
+  console.timeEnd('getShortestToChar1')
+
+})();
+
+;(function () {
+
+  function getShortestToChar (s, c) {
+    const charArr = [] // [ 3, 5, 6, 11 ]
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === c) {
+        charArr.push(i)
+      }
+    }
+    
+    const sArr = s.split('')
+    const arr = []
+    sArr.forEach((item, index) => {
+      let temp = s.length
+      charArr.forEach((item1, index1) => {
+        temp = Math.min(Math.abs(item1 - index), temp)
+      })
+      arr.push(temp)
+    })
+
+    return arr
+  }
+
+  // for 循环比 while + regexp 的效率更高些
+  console.time('getShortestToChar2')
+  console.log(getShortestToChar(s, c))
+  console.timeEnd('getShortestToChar2')
 
 })();
